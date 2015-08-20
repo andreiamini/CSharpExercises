b1215324450014b76a96c608d2963208f0bd4129
Liza Amini <liza.amini@gmail.com>
2015-08-20 18:50:37 +0100

    File cleaning

--- Sheet-11/Exercise11A.cs
+++ Sheet-11/Exercise11A.cs
@@ -1,1 +1,21 @@
+﻿using System;
+
+namespace Exercise11A
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Enter a decimal number: ");
+			int number = Convert.ToInt32 (Console.ReadLine ());
+
+			Console.WriteLine ("Decimal " + number + " = Hexadecimal " + dec_to_hex (number));
+		}
+
+		public static String dec_to_hex(int dec){
+			String hex = Convert.ToString(dec, 16);
+			return hex;
+		} 
+	}
+}
 
--- Sheet-9/Exercise9A.cs
+++ Sheet-9/Exercise9A.cs
@@ -1,1 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace Exercise9AA
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int number = 10000;
+			sieve (number);
+		}
+
+		public static void sieve(int numbers) { 
+			bool[] primeOrNah = new bool[numbers];
+			for (int i = 2; i < numbers; i++)
+			{
+				primeOrNah[i] = true;
+			}
+
+			for (int j = 2; j < numbers; j++)
+			{
+				if (primeOrNah[j])
+				{
+					for (int p = 2; (p*j) < numbers; p++)
+					{
+						primeOrNah[p * j] = false;
+					}
+				}
+			}
+
+			for (int k = 0; k < primeOrNah.Length; k++) {
+				if (primeOrNah[k] == true) {
+					Console.WriteLine (k);
+				}
+			}
+		}
+	}
+}
